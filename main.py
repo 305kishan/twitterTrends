94626e9b74f830af1ad5c80ca4efe426f2fb15ea
Kishan Kumar <305kishan@gmail.com>
2023-01-15 19:45:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-
-print("Hi From Workflow 1")
-
 # import the module
 import os
-import csv
 import tweepy
 import psycopg2
 from pytz import timezone
@@ -14,20 +10,55 @@ ACCESS_SECRET = os.environ["ACCESS_SECRET"]
 CONSUMER_KEY = os.environ["CONSUMER_KEY"]
 CONSUMER_SECRET = os.environ["CONSUMER_SECRET"]
 
-
-# Create a new CSV file called "example.csv"
-with open("twitter_trends.csv", "w", newline="") as file:
-    # Create a CSV writer object
-    writer = csv.writer(file)
-    
-    # Write the header row
-    writer.writerow(["Name", "Age", "City"])
-    
-    # Write some data rows
-    writer.writerow(["Alice", 25, "New York"])
-    writer.writerow(["Bob", 30, "Los Angeles"])
-    writer.writerow(["Charlie", 35, "Chicago"])
+HOST_NAME = os.environ["HOST_NAME"]
+DATABASE = os.environ["DATABASE"]
+USER = os.environ["USER"]
+PASSWORD = os.environ["PASSWORD"]
 
 
+auth = tweepy.OAuthHandler(ACCESS_KEY, ACCESS_SECRET)
+auth.set_access_token(CONSUMER_KEY, CONSUMER_SECRET)
+api = tweepy.API(auth)
 
-print("Hi From Workflow 2")
+woeid_india = 2282863
+woeid_us = 2357024
+woeid_world = 1
+
+trends_india = api.get_place_trends(id = woeid_india)
+trends_us = api.get_place_trends(id = woeid_us)
+trends_worldwide = api.get_place_trends(id = woeid_world)
+
+current_time = datetime.now().replace(microsecond=0, second=0, minute=0)
+local_tz = timezone("Asia/Calcutta")
+current_time = local_tz.localize(current_time)
+
+conn = psycopg2.connect(
+    host=HOST_NAME,
+    database=DATABASE,
+    user=USER,
+    password=PASSWORD)
+
+
+# Create a cursor
+cur = conn.cursor()
+
+for i in range(3):
+    if i == 0:
+        trends = trends_india
+        region = 'India'
+    if i == 1:
+        trends = trends_us
+        region = 'USA'
+    if i == 2:
+        trends = trends_worldwide
+        region = 'Worldwide'
+    for value in trends:
+        for trend in value['trends']:
+            record = (current_time, region, trend['name'], trend['tweet_volume'])
+            query = "INSERT INTO twitter_trends VALUES (%s, %s, %s, %s)"
+            cur.execute(query, record)
+            conn.commit()
+
+
+cur.close()
+conn.close()
