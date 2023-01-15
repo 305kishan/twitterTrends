b2a2a8103d52d5cecff037f287d6a36529d2c826
Kishan Kumar <305kishan@gmail.com>
2023-01-15 13:54:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ print("Hi From Workflow 1")
 
 # import the module
 import os
+import csv
 import tweepy
 import psycopg2
 from pytz import timezone
@@ -13,9 +14,19 @@ ACCESS_SECRET = os.environ["ACCESS_SECRET"]
 CONSUMER_KEY = os.environ["CONSUMER_KEY"]
 CONSUMER_SECRET = os.environ["CONSUMER_SECRET"]
 
-print(ACCESS_KEY)
-print(ACCESS_SECRET)
-print(CONSUMER_KEY)
-print(CONSUMER_SECRET)
+
+# Create a new CSV file called "example.csv"
+with open("twitter_trends.csv", "w", newline="") as file:
+    # Create a CSV writer object
+    writer = csv.writer(file)
+    
+    # Write the header row
+    writer.writerow(["Name", "Age", "City"])
+    
+    # Write some data rows
+    writer.writerow(["Alice", 25, "New York"])
+    writer.writerow(["Bob", 30, "Los Angeles"])
+    writer.writerow(["Charlie", 35, "Chicago"])
+
 
 print("Hi From Workflow 2")
